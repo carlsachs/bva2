532ef4a31b7bd286d99bd2334fb783bbbfe7687e
JSapp.me <herve76@gmail.com>
2021-04-30 16:15:47 -0600

    theme

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,6 @@
 <template>
-  <main
-    class="px-4 py-4 text-center text-gray-700 bg-gray-100 dark:bg-gray-600 dark:text-gray-200"
-  >
+  <main class="px-4 py-4 text-center bg-gray-600 text-gray-200">
+    
     <Toolbar />
 
     <div class="m-auto mb-5 text-sm text-center opacity-25">
