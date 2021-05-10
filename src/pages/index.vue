73f3cf31b974a427b4537ed4310729ba468e0db3
JSapp.me <herve76@gmail.com>
2021-05-10 08:37:39 -0600

    no color chart

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -23,70 +23,69 @@ export default defineComponent({
             data: [30, 40, 45, 50, 49, 60, 70, 99],
         }],
         chartOptions: {
-        chart: {
-          width: "100%",
-          id: "bitcoin-chart",
-          type: "area",
-          stacked: false,
-          zoom: {
-            enabled: false,
-          },
-        },
-        colors: ["#02182B"],
-        dataLabels: {
-          enabled: false,
-        },
-        markers: {
-          size: 0,
-        },
-        fill: {
-          type: "gradient",
-          gradient: {
-            shadeIntensity: 1,
-            inverseColors: false,
-            opacityFrom: 0.45,
-            opacityTo: 0.05,
-            stops: [20, 100, 100, 100],
-          },
-        },
-        xaxis: {
-          categories: [
-            1578798000000,
-            1581476400000,
-            1583982000000,
-            1586660400000,
-            1589252400000,
-            1591930800000,
-            1594522800000,
-            1597201200000,
-          ],
-          type: "datetime",
-          axisBorder: {
-            show: false,
-          },
-          axisTicks: {
-            show: false,
-          },
-          tooltip: {
-            shared: true,
-          },
-          legend: {
-            position: "top",
-            horizontalAlign: "right",
-            offsetX: -10,
-          },
-        },
-        yaxis: {
-          labels: {
-            formatter: function (value) {
-              return new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "BRL",
-              }).format(value);
+            chart: {
+                width: "100%",
+                id: "bitcoin-chart",
+                type: "area",
+                stacked: false,
             },
-          },
-        },
-      }
+            //colors: ["#02182B"],
+            dataLabels: {
+                enabled: false,
+            },
+            markers: {
+                size: 0,
+            },
+            fill: {
+                type: "gradient",
+                gradient: {
+                    shadeIntensity: 1,
+                    inverseColors: false,
+                    opacityFrom: 0.45,
+                    opacityTo: 0.05,
+                    stops: [20, 100, 100, 100],
+                },
+            },
+            xaxis: {
+                categories: [
+                    1578798000000,
+                    1581476400000,
+                    1583982000000,
+                    1586660400000,
+                    1589252400000,
+                    1591930800000,
+                    1594522800000,
+                    1597201200000,
+                ],
+                type: "datetime",
+                axisBorder: {
+                    show: false,
+                },
+                axisTicks: {
+                    show: false,
+                },
+                tooltip: {
+                    shared: true,
+                },
+                /*
+                legend: {
+                    position: "top",
+                    horizontalAlign: "right",
+                    offsetX: -10,
+                },
+                */
+            },
+            yaxis: {
+                labels: {
+                    formatter: function (value) {
+                        return new Intl.NumberFormat("en-US", {
+                            style: "currency",
+                            currency: "USD",
+                        }).format(value);
+                    },
+                },
+            },
+        }
     })
 
     onMounted(() => {
