3782ff0e393788079b64386a1154f3f7db5f85c7
JSapp.me <herve76@gmail.com>
2021-05-20 09:52:02 -0600

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,8 +4,8 @@
         <p class="text-white">Welcome to Bitcoin vs. Alts, ...</p>
     </div>
     <div class="mx-4 my-14 pt-4 border-2 border-blue-900 rounded-lg">
-        <apexchart type="area" height="350" width="350" :options="chartOptions" :series="series"></apexchart>
-        <apexchart type="area" height="450" width="450" :options="chartOptions" :series="series"></apexchart>
+        <apexchart type="area" height="500" width="500" :options="chartOptions" :series="series"></apexchart>
+        <apexchart type="area" height="500" width="500" :options="chartOptions" :series="series"></apexchart>
         <apexchart type="area" height="500" width="500" :options="chartOptions" :series="series"></apexchart>
     </div>
     <div class="mx-4 my-14 p-4 border-2 border-blue-900 rounded-lg text-white relative">
