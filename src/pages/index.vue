9cb24e5f6af80cd4f1d396f84dbdae8bfb5ab101
JSapp.me <herve76@gmail.com>
2021-05-21 13:56:09 -0600

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -43,93 +43,63 @@
 
         </div>
 
+
         <div class="mt-4 p-4">
             <div class="flex flex-col">
                 <div class="-my-2 overflow-x-auto">
                     <div class="py-2 align-middle inline-block min-w-full">
-                        <div class=" overflow-hidden border-2 border-blue-900 rounded-lg">
+                        <div class=" overflow-hidden border-1 border-blue-900 rounded-lg">
                             <table class="min-w-full divide-y divide-gray-200">
                                 <thead>
                                     <tr>
                                         <th class="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                                             <div class="flex cursor-pointer">
-                                                <span class="mr-2">PRODUCT NAME</span>
+                                                <span class="mr-2">PAIR</span>
                                             </div>
                                         </th>
-                                        <th
-                                            class="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                        <th class="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                                             <div class="flex cursor-pointer">
-                                                <span class="mr-2">Stock</span>
+                                                <span class="mr-2">PNL</span>
                                             </div>
                                         </th>
-                                        <th
-                                            class="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                        <th class="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                                             <div class="flex cursor-pointer">
-                                                <span class="mr-2">STATUS</span>
+                                                <span class="mr-2">TYPE</span>
                                             </div>
                                         </th>
-                                        <th
-                                            class="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                        <th class="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                                             <div class="flex cursor-pointer">
-                                                <span class="mr-2">ACTION</span>
+                                                <span class="mr-2">BUY PRICE</span>
+                                            </div>
+                                        </th>
+                                        <th class="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                            <div class="flex cursor-pointer">
+                                                <span class="mr-2">SELL PRICE</span>
                                             </div>
                                         </th>
                                     </tr>
                                 </thead>
-                                <tbody class="divide-y divide-gray-200">
+                                <tbody class="divide-y divide-gray-200" v-for="(row, i) in rows">
                                     <tr>
-                                        <td
-                                            class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
-                                            <p>Apple MacBook Pro 13</p>
-                                            <p class="text-xs text-gray-400">PC & Laptop
-                                            </p>
+                                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                            <p>{{ row.pair }}</p>
                                         </td>
-                                        <td
-                                            class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
-                                            <p>77</p>
+                                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                            <p>{{ Number(row.pnl).toFixed(2) }}%</p>
                                         </td>
-                                        <td
-                                            class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
                                             <div class="flex text-green-500">
-                                                <svg xmlns="http://www.w3.org/2000/svg"
-                                                    class="w-5 h-5 mr-1" fill="none"
-                                                    viewBox="0 0 24 24"
-                                                    stroke="currentColor">
-                                                    <path stroke-linecap="round"
-                                                        stroke-linejoin="round"
-                                                        stroke-width="2"
-                                                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                                </svg>
-                                                <p>Active</p>
+                                                <p>{{ row.type }}</p>
                                             </div>
                                         </td>
-                                        <td
-                                            class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
-                                            <div class="flex space-x-4">
-                                                <a href="#" class="text-blue-500 hover:text-blue-600">
-                                                <svg xmlns="http://www.w3.org/2000/svg"
-                                                    class="w-5 h-5 mr-1"
-                                                    fill="none" viewBox="0 0 24 24"
-                                                    stroke="currentColor">
-                                                    <path stroke-linecap="round"
-                                                        stroke-linejoin="round"
-                                                        stroke-width="2"
-                                                        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                                </svg>
-                                                <p>Edit</p>
-                                                </a>
-                                                <a href="#" class="text-red-500 hover:text-red-600">
-                                                <svg xmlns="http://www.w3.org/2000/svg"
-                                                    class="w-5 h-5 mr-1 ml-3"
-                                                    fill="none" viewBox="0 0 24 24"
-                                                    stroke="currentColor">
-                                                    <path stroke-linecap="round"
-                                                        stroke-linejoin="round"
-                                                        stroke-width="2"
-                                                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                                </svg>
-                                                <p>Delete</p>
-                                                </a>
+                                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                            <div class="flex text-green-500">
+                                                <p>{{ row.buy_price }}</p>
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                            <div class="flex text-green-500">
+                                                <p>{{ row.sell_price }}</p>
                                             </div>
                                         </td>
                                     </tr>
@@ -141,9 +111,8 @@
             </div>
         </div>
 
-    </div>
 
-    <!--p>{{ user }}</p-->
+    </div>
   </div>
 </template>
 
@@ -163,6 +132,7 @@ export default defineComponent({
         strat_lifetime: 0,
         win_rate: 0,
         user: null,
+        rows: [],
         ///////// ///////// ///////// /////////
         series: [
             {
@@ -258,6 +228,8 @@ export default defineComponent({
                 let tpnl_bva = []
                 let pnl_btc = 0
                 let pnl_bva = 0
+                
+                state.rows = bvas.data
 
                 for ( var btc of btcs.data ) {
                     pnl_btc = 100 * (Number(btc[4]) - Number(btc[1])) / Number(btc[1]) + pnl_btc
