344328329bbb536f7cbb003d3ec17d19283a5ed5
JSapp.me <herve76@gmail.com>
2021-05-21 13:31:10 -0600

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -47,38 +47,36 @@
             <div class="flex flex-col">
                 <div class="-my-2 overflow-x-auto">
                     <div class="py-2 align-middle inline-block min-w-full">
-                        <div
-                            class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg bg-white">
+                        <div class=" overflow-hidden border-2 border-blue-900 rounded-lg">
                             <table class="min-w-full divide-y divide-gray-200">
                                 <thead>
                                     <tr>
-                                        <th
-                                            class="px-6 py-3 bg-gray-50 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                        <th class="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                                             <div class="flex cursor-pointer">
                                                 <span class="mr-2">PRODUCT NAME</span>
                                             </div>
                                         </th>
                                         <th
-                                            class="px-6 py-3 bg-gray-50 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                            class="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                                             <div class="flex cursor-pointer">
                                                 <span class="mr-2">Stock</span>
                                             </div>
                                         </th>
                                         <th
-                                            class="px-6 py-3 bg-gray-50 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                            class="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                                             <div class="flex cursor-pointer">
                                                 <span class="mr-2">STATUS</span>
                                             </div>
                                         </th>
                                         <th
-                                            class="px-6 py-3 bg-gray-50 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                            class="px-6 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                                             <div class="flex cursor-pointer">
                                                 <span class="mr-2">ACTION</span>
                                             </div>
                                         </th>
                                     </tr>
                                 </thead>
-                                <tbody class="bg-white divide-y divide-gray-200">
+                                <tbody class="divide-y divide-gray-200">
                                     <tr>
                                         <td
                                             class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
