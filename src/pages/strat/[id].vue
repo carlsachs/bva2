63cec03f3fce22bdef56b921bd290fb89ed92659
JSapp.me <herve76@gmail.com>
2021-05-26 14:45:06 -0600

    Update [id].vue

--- src/pages/strat/[id].vue
+++ src/pages/strat/[id].vue
@@ -33,7 +33,7 @@
                 </div>
 
                 <router-link to="/profile#apikey" class="group flex items-center bg-indigo-900 bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5 cursor-pointer hover:bg-blue-900 hover:bg-opacity-100 transition">
-                    <div class="flex-auto">Subscribe</div>
+                    <div class="flex-auto text-green-500 font-semibold">Subscribe</div>
                 </router-link>
             </div>
             
