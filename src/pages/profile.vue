068cc0378692bb4f2e189c3f0f2efa0d6c4ae516
Herve Fulchiron <contact@jsapp.me>
2021-06-15 14:53:53 -0600

    Update profile.vue

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="auth0.state.isAuthenticated && auth0.state.user" class="text-center text-gray-300">
 
-    <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+    <div class="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
       <div v-for="(subscription, i) in Object.values(subscriptions)" :class="{ 'bg-indigo-900 bg-opacity-20': subs?.findIndex(sub => (sub.code === subscription.code)) >= 0 }" :key="subscription.code" class="mx-4 my-4 p-4 border-2 border-blue-900 rounded-lg text-white relative">
         <div class="text-5xl font-extrabold text-blue-600"><b>{{ subscription.name }}</b></div>
         <hr class="w-5 mx-auto border-blue-400 my-8">
@@ -44,16 +44,19 @@
                 placeholder="your api key"
                 aria-label="key"
                 type="text"
+                size=50
                 autocomplete="false"
                 class="my-3 px-4 py-2 text-sm text-center bg-gray-900 border rounded outline-none active:outline-none border-gray-700"
                 @input="resetInput"
-              >&nbsp;
+              >
+              <br>
               <input
                 v-model="subs[subs?.findIndex(sub => (sub.code == subscription.code))].secret"
                 placeholder="your api secret"
                 aria-label="secret"
                 type="text"
                 autocomplete="false"
+                size=50
                 class="my-3 px-4 py-2 text-sm text-center bg-gray-900 border rounded outline-none active:outline-none border-gray-700"
                 @input="resetInput"
               >
