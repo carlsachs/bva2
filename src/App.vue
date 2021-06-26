01507c6a954007fbc676befd59f8ec613f93cb45
Herve Fulchiron <contact@jsapp.me>
2021-06-26 12:34:56 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
   </div>
   <div class="text-center text-gray-300 p-5 m-6 flex flex-row items-center justify-center p-2 space-x-5 mb-5">
     <div class="text-green-500">{{ btc && Number(btc.price).toFixed(2) }} usd</div> &nbsp;
-    Bitcoin Vs. Alts 2019 – 2021 ©
+    Bitcoin Vs. Alts © 2019 – 2021
   </div>
   <div class="text-center text-xs text-indigo-300 p-5 m-2 flex flex-row items-center justify-center p-2 space-x-5 mb-5">
     {{ stats.timing }} secs
