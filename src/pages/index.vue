15c2c799357f05be72ab6c90af6846fbdc1e5081
Herve Fulchiron <contact@jsapp.me>
2021-06-29 12:52:42 -0600

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,7 +5,7 @@
             Bitcoin Vs. Alts, BVA, is a marketplace offering subscriptions to the best crypto trading strategies. 
         </div>
         <div class="text-xl text-cyan-200 mt-10">
-            It was created to fund the #1 open <a href='https://www.investopedia.com/terms/b/backtesting.asp' target="_new"><s>back</s></a> <a href='https://www.investopedia.com/articles/trading/10/backtesting-walkforward-important-correlation.asp' target="_new">forward testing</a> <a href="https://bitcoinvsalts.com" target="_new" class="underline">ecosystem</a>.
+            It was created to fund the #1 open <a href='https://www.investopedia.com/articles/trading/10/backtesting-walkforward-important-correlation.asp' target="_new">forward testing</a> <a href="https://bitcoinvsalts.com" target="_new" class="underline">ecosystem</a>.
         </div>
         <Hero/>
 
