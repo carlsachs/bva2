2634e98a7a47ad8a9236be576feafeec60de3ae3
Herve Fulchiron <contact@jsapp.me>
2021-08-02 12:52:35 -0600

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,9 +6,6 @@
                     Bitcoin Vs. Alts, BVA, is a marketplace offering subscriptions to the best crypto trading strategies.
                 </div>
                 <div class="text-xl text-cyan-200 mt-3">
-                    Just let our robot trader manage your crypto portfolio for you.
-                </div>
-                <div class="text-xl text-cyan-200 mt-3">
                     This marketplace was created to fund the #1 <a href='https://bitcoinvsaltcoins.com' target="_new" class="underline">open forward testing platform</a>.
                 </div>
                 <div class="text-xl text-cyan-200 mt-3">
