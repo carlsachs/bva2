07d6daed50f193c813f2257ee4b735c5dd9206e7
Herve Fulchiron <contact@jsapp.me>
2021-08-02 11:43:50 -0600

    f4f added

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -11,90 +11,95 @@
             This marketplace was created to fund the #1 <a href='https://bitcoinvsaltcoins.com' target="_new" class="underline">open forward testing platform</a>.
         </div>
         <div class="text-xl text-cyan-200 mt-3">
-            We are currently tracking more than <router-link to="/signals" class="underline">1000 strategies</router-link>. Here are our best performing strategies:
+            We are currently tracking more than <router-link to="/signals" class="underline">1000 strategies</router-link>.
+        </div>
+        <div class="text-xl text-cyan-200 mt-3">
+            Here are our curated strategies.
         </div>
 
-        <div v-for="(strategy, i) in strategies" :key="strategy.name" class="mx-2 my-7 py-4 border-2 border-blue-900 rounded-lg text-white relative">
-            
-            <h1 class="mb-0 text-5xl text-uppercase font-semibold">{{ strategy.name }}</h1>
+        <div class="p-4 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-1 sm:gap-5">
+            <div v-for="(strategy, i) in strategies" :key="strategy.name" class="mx-2 my-4 py-4 border-2 border-blue-900 rounded-lg text-white relative">
+                
+                <h1 class="mb-0 text-3xl text-uppercase font-semibold">{{ strategy.name }}</h1>
 
-            <apexchart type="area" height="400" :options="chartOptions" :series="strategy.series"></apexchart>
+                <apexchart type="area" height="400" :options="chartOptions" :series="strategy.series"></apexchart>
 
-            <div class="m-7 text-3xl">{{ strategy.description }}</div>
+                <div class="m-7 text-3xl">{{ strategy.description }}</div>
 
-            <div v-if="!auth0.state.isAuthenticated" @click="login" class="mx-10 p-4 bg-indigo-900 text-orange-500 font-bold group flex-auto items-center shadow-xl gap-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5 cursor-pointer hover:bg-blue-900 hover:bg-opacity-100 transition">
-                {{ strategy.price }} 
+                <div v-if="!auth0.state.isAuthenticated" @click="login" class="mx-10 p-4 bg-indigo-900 text-orange-500 font-bold group flex-auto items-center shadow-xl gap-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5 cursor-pointer hover:bg-blue-900 hover:bg-opacity-100 transition">
+                    {{ strategy.price }} 
+                </div>
+                <div class="p-4 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-1 sm:gap-5 uppercase">
+                    <div class="flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5">
+                        <div class="flex-auto">Total PnL</div>
+                        <div class="flex-auto text-justify text-blue-300 block">{{ Number(strategy.total_pnl * 15).toFixed(2) }}%</div>
+                    </div>
+
+                    <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5">
+                        <div class="flex-auto">Max. Concurrent Trades</div>
+                        <div class="flex-auto text-justify text-blue-300 block">{{strategy.max_concurrent}}</div>
+                    </div>
+
+                    <div class="flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5">
+                        <div class="flex-auto">Portfolio PnL*</div>
+                        <div class="flex-auto text-justify text-blue-300 block">{{ strategy.total_pnl }}%</div>
+                    </div>
+
+                    <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5">
+                        <div class="flex-auto">Trades Count</div>
+                        <div class="flex-auto text-justify text-blue-300 block">{{ strategy.total_signals }}</div>
+                    </div>
+
+                    <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5">
+                        <div class="flex-auto">Avg. Profit per Trade</div>
+                        <div class="flex-auto text-justify text-blue-300 block">{{ strategy.avg_pnl }}%</div>
+                    </div>
+
+                    <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5">
+                        <div class="flex-auto">Win Rate</div>
+                        <div class="flex-auto text-justify text-blue-300 block">{{ strategy.win_rate }}%</div>
+                    </div>
+
+                    <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5">
+                        <div class="flex-auto">Verif. Trade History</div>
+                        <div class="flex-auto text-justify text-blue-300 block">{{ strategy.strat_lifetime }} days</div>
+                    </div>
+
+                    <router-link :to="'/strat/'+strategy.id" class="group bg-indigo-900 flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5 cursor-pointer hover:bg-blue-900 hover:bg-opacity-100 transition">
+                        <div class="flex-auto font-bold">See All Signals</div>
+                    </router-link>
+                </div>
+
+                <div class="mt-5 italic">* PNL calculated using 1/{{strategy.max_concurrent}} of the whole BTC amount for each trade as recommended.</div>
             </div>
-            <div class="p-4 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 sm:gap-5 uppercase">
-                <div class="flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5">
-                    <div class="flex-auto">Total PnL</div>
-                    <div class="flex-auto text-justify text-blue-300 block">{{ Number(strategy.total_pnl * 15).toFixed(2) }}%</div>
-                </div>
-
-                <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5">
-                    <div class="flex-auto">Max. Concurrent Trades</div>
-                    <div class="flex-auto text-justify text-blue-300 block">{{strategy.max_concurrent}}</div>
-                </div>
-
-                <div class="flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5">
-                    <div class="flex-auto">Portfolio PnL*</div>
-                    <div class="flex-auto text-justify text-blue-300 block">{{ strategy.total_pnl }}%</div>
-                </div>
-
-                <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5">
-                    <div class="flex-auto">Trades Count</div>
-                    <div class="flex-auto text-justify text-blue-300 block">{{ strategy.total_signals }}</div>
-                </div>
-
-                <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5">
-                    <div class="flex-auto">Avg. Profit per Trade</div>
-                    <div class="flex-auto text-justify text-blue-300 block">{{ strategy.avg_pnl }}%</div>
-                </div>
-
-                <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5">
-                    <div class="flex-auto">Win Rate</div>
-                    <div class="flex-auto text-justify text-blue-300 block">{{ strategy.win_rate }}%</div>
-                </div>
-
-                <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5">
-                    <div class="flex-auto">Verif. Trade History</div>
-                    <div class="flex-auto text-justify text-blue-300 block">{{ strategy.strat_lifetime }} days</div>
-                </div>
-
-                <router-link :to="'/strat/'+strategy.id" class="group bg-indigo-900 flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5 cursor-pointer hover:bg-blue-900 hover:bg-opacity-100 transition">
-                    <div class="flex-auto font-bold">See All Signals</div>
-                </router-link>
-            </div>
-
-            <div class="mt-5 italic">* PNL calculated using 1/{{strategy.max_concurrent}} of the whole BTC amount for each trade as recommended.</div>
         </div>
 
         <div class="p-4 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-1 sm:gap-5">
             <div class="mx-2 my-4 py-4 border-2 border-blue-900 rounded-lg text-white relative">
-            <a href="https://www.binance.com/?ref=W5BD94FW" target="_new">
-                <img width=200 class="mx-auto mb-5" alt="BvA Discord" src="/binance.svg" />
-                <div class="text-3xl text-white">Official Binance Broker</div>
-            </a>
+                <a href="https://www.binance.com/?ref=W5BD94FW" target="_new">
+                    <img width=200 class="mx-auto mb-5" alt="BvA Discord" src="/binance.svg" />
+                    <div class="text-3xl text-white">Official Binance Broker</div>
+                </a>
             </div>
             <div class="mx-2 my-4 py-4 border-2 border-blue-900 rounded-lg text-white relative">
-            <a href="https://bitcoinvsaltcoins.com" target="_new">
-                <img width=132 class="mx-auto mb-5" alt="Strategy Incubator" src="/creator.png" />
-                <div class="text-3xl text-white">Our Strategy Incubator</div>
-            </a>
+                <a href="https://bitcoinvsaltcoins.com" target="_new">
+                    <img width=132 class="mx-auto mb-5" alt="Strategy Incubator" src="/creator.png" />
+                    <div class="text-3xl text-white">Our Strategy Incubator</div>
+                </a>
             </div>
         </div>
         <div class="p-4 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-1 sm:gap-5">
             <div class="mx-2 my-4 py-4 border-2 border-blue-900 rounded-lg text-white relative">
-            <a href="https://discordapp.com/invite/4EQrEgj" target="_new">
-                <img width=132 class="mx-auto mb-5" alt="BvA Discord" src="/discord.png" />
-                <div class="text-3xl text-white">1200+ Discord Members</div>
-            </a>
+                <a href="https://discordapp.com/invite/4EQrEgj" target="_new">
+                    <img width=132 class="mx-auto mb-5" alt="BvA Discord" src="/discord.png" />
+                    <div class="text-3xl text-white">1200+ Discord Members</div>
+                </a>
             </div>
             <div class="mx-2 my-4 py-4 border-2 border-blue-900 rounded-lg text-white relative">
-            <a href="https://github.com/bitcoinvsalts/node-binance-trader" target="_new">
-                <img width=102 class="mx-auto mt-3 mb-10" alt="BvA GitHub" src="/github.png" />
-                <div class="text-3xl text-white">840+ GitHub Stars</div>
-            </a>
+                <a href="https://github.com/bitcoinvsalts/node-binance-trader" target="_new">
+                    <img width=102 class="mx-auto mt-3 mb-10" alt="BvA GitHub" src="/github.png" />
+                    <div class="text-3xl text-white">840+ GitHub Stars</div>
+                </a>
             </div>
         </div>
 
@@ -162,6 +167,19 @@ export default defineComponent({
                 strat_lifetime: 0,
                 win_rate: 0,
                 max_concurrent: 20,
+                price: "Limited Availability 0.07 BNB",
+            },
+            {
+                id: 948,
+                name: "Fist4Fun.V02.ENJUSDT",
+                series: [{ name: "F4F", data: [] }],
+                description: "A very special and profitable One Margin Pair strategy.",
+                total_pnl: 0,
+                total_signals: 0,
+                avg_pnl: 0,
+                strat_lifetime: 0,
+                win_rate: 0,
+                max_concurrent: 1,
                 price: "Limited Availability 0.07 BNB",
             }
         ],
