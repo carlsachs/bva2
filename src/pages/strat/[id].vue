61ec780cc87159a18df9b7d54582b3ec84cdafbf
Herve Fulchiron <contact@jsapp.me>
2021-08-02 15:21:04 -0600

    Update [id].vue

--- src/pages/strat/[id].vue
+++ src/pages/strat/[id].vue
@@ -9,37 +9,37 @@
             <div v-if="description" class="m-7 text-xl">{{ description }}</div>
             
             <div class="p-4 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 sm:gap-5 uppercase">
-                <div class="flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5 transition">
+                <div class="flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5  mt-5 transition">
                     <div class="flex-auto">Total PnL</div>
                     <div class="flex-auto text-justify text-blue-300 block">{{ Number(total_pnl).toFixed(2) }}%</div>
                 </div>
 
-                <div v-if="max_concurrent>1" class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5 transition">
+                <div v-if="max_concurrent>1" class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5  mt-5 transition">
                     <div class="flex-auto">Max. Concurrent Trades</div>
                     <div class="flex-auto text-justify text-blue-300 block">{{ max_concurrent }}</div>
                 </div>
 
-                <div v-if="max_concurrent>1" class="flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5 transition">
+                <div v-if="max_concurrent>1" class="flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5  mt-5 transition">
                     <div class="flex-auto">Portfolio PnL*</div>
                     <div class="flex-auto text-justify text-blue-300 block">{{ (Number(total_pnl)/Number(max_concurrent)).toFixed(2) }}%</div>
                 </div>
 
-                <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5 transition">
+                <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5  mt-5 transition">
                     <div class="flex-auto">Trades Count</div>
                     <div class="flex-auto text-justify text-blue-300 block">{{ total_signals }}</div>
                 </div>
 
-                <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5 transition">
+                <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5  mt-5 transition">
                     <div class="flex-auto">Avg. Profit per Trade</div>
                     <div class="flex-auto text-justify text-blue-300 block">{{ avg_pnl }}%</div>
                 </div>
 
-                <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5 transition">
+                <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5  mt-5 transition">
                     <div class="flex-auto">Win Rate</div>
                     <div class="flex-auto text-justify text-blue-300 block">{{ win_rate }}%</div>
                 </div>
 
-                <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5 transition">
+                <div class="group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5  mt-5 transition">
                     <div class="flex-auto">Verif. Trade History</div>
                     <div class="flex-auto text-justify text-blue-300 block">{{ strat_lifetime }} days</div>
                 </div>
