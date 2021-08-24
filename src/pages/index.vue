08250cc562a128c359c77aab95e52c72c3ea6779
Herve Fulchiron <contact@jsapp.me>
2021-08-24 11:02:27 -0600

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,7 +6,7 @@
                     Bitcoin vs. Alts is an algo crypto trading marketplace.
                 </div>
                 <div class="text-xl text-cyan-200 mt-3">
-                    This marketplace was created to fund the #1 <a href='https://bitcoinvsaltcoins.com' target="_new" class="underline">open forward testing platform</a>.
+                    It was created to fund the #1 <a href='https://bitcoinvsaltcoins.com' target="_new" class="underline">open forward testing platform</a>.
                 </div>
                 <div class="text-xl text-cyan-200 mt-3">
                     We are currently tracking more than <router-link to="/signals" class="underline">1200 strategies</router-link>.
