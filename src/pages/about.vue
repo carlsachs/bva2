6e6bd765e58ab76ef475bf3288554338c33f6420
JSapp.me <herve76@gmail.com>
2021-08-28 12:52:35 -0600

    Update about.vue

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="text-xl text-cyan-200 mt-10">
-            We are a team passionated in algo crypto trading. 
+            We are a team passionated in algo crypto trading. Our goal is to democratize access to the best crypto algo trading strategies.
         </div>
         <div class="text-xl text-cyan-200 mt-10">
             We use the latest Machine Learning practices to train our prediction models.
