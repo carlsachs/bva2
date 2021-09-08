ad7741c3d0fadff639e2ac9436e1c6adeb2ce8d9
JSapp.me <herve76@gmail.com>
2021-09-08 09:40:14 -0600

    Update topstrats.vue

--- src/pages/topstrats.vue
+++ src/pages/topstrats.vue
@@ -1,24 +1,45 @@
 <template>
     <div>
-        <h1 class="text-xl text-white text-uppercase font-semibold">Top Strategies for the past 7 days</h1>
-        <div v-if="!strats" class="my-0 text-gray-300">Loading... <img class="mx-auto mb-5" src="/spinner.svg" /></div>
-        <div class="mx-2 my-7 py-4 border-2 border-blue-900 rounded-lg text-white relative">
-            <div v-if="true" class="mt-4 p-4">
+
+        <div class="p-4">
+            <div class="mx-2 my-4 py-4 border-2 border-blue-900 rounded-lg text-white relative">
+                <div class="text-2xl text-cyan-200 mt-3">
+                    Our goal is to democratize access to the best crypto trading algorithms. 
+                </div>
+                <!--div class="text-xl text-cyan-200 mt-3">
+                    We created this marketplace to fund the #1 <a href='https://bitcoinvsaltcoins.com' target="_new" class="underline">open forward testing platform</a>.
+                </div-->
+                <div class="text-xl text-cyan-200 mt-3">
+                    We are currently tracking more than <router-link to="/signals" class="underline">1300 strategies</router-link>.
+                </div>
+                <div class="text-cyan-200 mt-3">
+                    <u>Disclaimer</u>: There is a very high degree of risk involved in trading crypto. 
+                    Past results are not indicative of future returns.
+                    This platform is for educational purposes only.
+                </div>
+            </div>
+        </div>
+
+        <h1 class="text-xl text-white text-uppercase font-semibold mt-6">Top Strategies for the past 7 days</h1>
+        <div v-if="!strats" class="my-4 text-gray-300">Loading... <img class="mx-auto mb-5" src="/spinner.svg" /></div>
+        <div class="p-4 mt-4">
+            <div class="mx-2">
                 <div class="flex flex-col">
                     <div class="-my-2 overflow-x-auto">
                         <div class="py-2 align-middle inline-block min-w-full">
                             <div class=" overflow-hidden border-1 border-blue-900 rounded-lg">
                                 <table class="min-w-full divide-y divide-gray-200">
-                                    <tbody v-if="strats" class="divide-y divide-gray-200 hover:bg-blue-900 hover:bg-opacity-40 visited:bg-blue-900 visited:bg-opacity-40" v-for="(row, i) in strats" :key="row.id">
+                                    <tbody v-if="strats" class="divide-y divide-gray-200 visited:bg-blue-900 visited:bg-opacity-40" v-for="(row, i) in strats" :key="row.id">
                                         <tr>
                                             <td class="px-6 py-4 text-gray-300 font-bold whitespace-no-wrap text-sm leading-5">
-                                                <h1 class="text-xl text-white"><router-link :to="/strat/+row.id">{{ row.stratname }}</router-link></h1>
-                                                {{ row.max_concurrent }}
-                                                {{ row.description }}
-                                                <div :class="{ 'text-green-500': Number(row.sumpnl)>0, 'text-red-500': Number(row.sumpnl)<0 }">{{ Number(row.sumpnl).toFixed(2) }}%</div>
+                                                <div :class="{ 'text-green-500': Number(row.sumpnl)>0, 'text-red-500': Number(row.sumpnl)<0 }" class="text-xl mb-5">{{ Number(row.sumpnl).toFixed(2) }}%</div>
                                                 <button v-if="row.forsale" @click="login" class="font-bold mx-auto text-xl items-center bg-indigo-900 bg-opacity-10 shadow-xl px-6 py-5 rounded-lg cursor-pointer hover:bg-opacity-100 transition">
-                                                    <div class="text-green-500 text-xl font-semibold">Subscribe and Auto Trade It Now</div>
+                                                    <div class="text-green-500 text-xl font-semibold">Subscribe</div>
                                                 </button>
+                                                <h1 :class="{ 'text-green-500': row.forsale, 'text-gray-500': !row.forsale }" class="mt-8 text-xl">
+                                                    {{ i+1 }}.&nbsp;<router-link :to="/strat/+row.id">{{ row.stratname }}</router-link>
+                                                </h1>
+                                                {{ row.description }}
                                             </td>
                                         </tr>
                                     </tbody>
@@ -26,9 +47,41 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> 
+            </div>
+        </div>  
+
+
+        <div class="p-4 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-1 sm:gap-5">
+            <div class="mx-2 my-4 py-4 border-2 border-blue-900 rounded-lg text-white relative">
+                <a href="https://www.binance.com/?ref=W5BD94FW" target="_new">
+                    <img width=200 class="mx-auto mb-5" alt="BvA Discord" src="/binance.svg" />
+                    <div class="text-2xl text-white">Official Binance Broker</div>
+                </a>
+            </div>
+            <div class="mx-2 my-4 py-4 border-2 border-blue-900 rounded-lg text-white relative">
+                <a href="https://bitcoinvsaltcoins.com" target="_new">
+                    <img width=132 class="mx-auto mb-5" alt="Strategy Incubator" src="/creator.png" />
+                    <div class="text-2xl text-white">Our Strategy Incubator</div>
+                </a>
             </div>
         </div>
+        <div class="p-4 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-1 sm:gap-5">
+            <div class="mx-2 my-4 py-4 border-2 border-blue-900 rounded-lg text-white relative">
+                <a href="https://discordapp.com/invite/4EQrEgj" target="_new">
+                    <img width=132 class="mx-auto mb-5" alt="BvA Discord" src="/discord.png" />
+                    <div class="text-2xl text-white">1200+ Discord Members</div>
+                </a>
+            </div>
+            <div class="mx-2 my-4 py-4 border-2 border-blue-900 rounded-lg text-white relative">
+                <a href="https://github.com/bitcoinvsalts/node-binance-trader" target="_new">
+                    <img width=102 class="mx-auto mt-3 mb-10" alt="BvA GitHub" src="/github.png" />
+                    <div class="text-2xl text-white">840+ GitHub Stars</div>
+                </a>
+            </div>
+        </div>
+
+
     </div>
 </template>
 
