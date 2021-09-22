60c4710d458b1b2ee719f646f05b4c6149c392cb
JSapp.me <herve76@gmail.com>
2021-09-22 10:55:27 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,37 +10,7 @@
                       value: '#121826'
                   }
               },
-              fpsLimit: 60,
-              interactivity: {
-                  detectsOn: 'canvas',
-                  events: {
-                      onClick: {
-                          enable: true,
-                          mode: 'push'
-                      },
-                      onHover: {
-                          enable: true,
-                          mode: 'repulse'
-                      },
-                      resize: true
-                  },
-                  modes: {
-                      bubble: {
-                          distance: 400,
-                          duration: 2,
-                          opacity: 0.8,
-                          size: 40,
-                          speed: 3
-                      },
-                      push: {
-                          quantity: 4
-                      },
-                      repulse: {
-                          distance: 200,
-                          duration: 0.4
-                      }
-                  }
-              },
+              fpsLimit: 40,
               particles: {
                   color: {
                       value: '#ffffff'
@@ -60,7 +30,7 @@
                       enable: true,
                       outMode: 'bounce',
                       random: false,
-                      speed: 6,
+                      speed: 3,
                       straight: false
                   },
                   number: {
@@ -95,37 +65,7 @@
                       value: '#121826'
                   }
               },
-              fpsLimit: 60,
-              interactivity: {
-                  detectsOn: 'canvas',
-                  events: {
-                      onClick: {
-                          enable: true,
-                          mode: 'push'
-                      },
-                      onHover: {
-                          enable: true,
-                          mode: 'repulse'
-                      },
-                      resize: true
-                  },
-                  modes: {
-                      bubble: {
-                          distance: 400,
-                          duration: 2,
-                          opacity: 0.8,
-                          size: 40,
-                          speed: 3
-                      },
-                      push: {
-                          quantity: 4
-                      },
-                      repulse: {
-                          distance: 200,
-                          duration: 0.4
-                      }
-                  }
-              },
+              fpsLimit: 40,
               particles: {
                   color: {
                       value: '#ffffff'
@@ -145,7 +85,7 @@
                       enable: true,
                       outMode: 'bounce',
                       random: false,
-                      speed: 6,
+                      speed: 3,
                       straight: false
                   },
                   number: {
