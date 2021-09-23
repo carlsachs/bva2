b4c780fbff113dc37aa0264dc3390373d637be47
JSapp.me <herve76@gmail.com>
2021-09-23 09:49:45 -0600

    interact

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,36 @@
                   }
               },
               fpsLimit: 40,
+              interactivity: {
+                  detectsOn: 'canvas',
+                  events: {
+                      onClick: {
+                          enable: true,
+                          mode: 'push'
+                      },
+                      onHover: {
+                          enable: true,
+                          mode: 'repulse'
+                      },
+                      resize: true
+                  },
+                  modes: {
+                      bubble: {
+                          distance: 400,
+                          duration: 2,
+                          opacity: 0.8,
+                          size: 40,
+                          speed: 3
+                      },
+                      push: {
+                          quantity: 4
+                      },
+                      repulse: {
+                          distance: 200,
+                          duration: 0.4
+                      }
+                  }
+              },
               particles: {
                   color: {
                       value: '#ffffff'
@@ -66,6 +96,36 @@
                   }
               },
               fpsLimit: 40,
+              interactivity: {
+                  detectsOn: 'canvas',
+                  events: {
+                      onClick: {
+                          enable: true,
+                          mode: 'push'
+                      },
+                      onHover: {
+                          enable: true,
+                          mode: 'repulse'
+                      },
+                      resize: true
+                  },
+                  modes: {
+                      bubble: {
+                          distance: 400,
+                          duration: 2,
+                          opacity: 0.8,
+                          size: 40,
+                          speed: 3
+                      },
+                      push: {
+                          quantity: 4
+                      },
+                      repulse: {
+                          distance: 200,
+                          duration: 0.4
+                      }
+                  }
+              },
               particles: {
                   color: {
                       value: '#ffffff'
