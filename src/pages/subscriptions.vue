91f481e867cdf6f79ca5cc1f0bd57541e193cabb
JSapp.me <herve76@gmail.com>
2021-09-24 08:29:40 -0600

    Update subscriptions.vue

--- src/pages/subscriptions.vue
+++ src/pages/subscriptions.vue
@@ -66,7 +66,7 @@
               <div class="flex items-center justify-center" v-if="subscription.mode==='FUTURE'">
                 <label :for="'toogles'+subscription.code" class="flex items-center cursor-pointer">
                   <div class="relative">
-                    <input :id="'toogles'+subscription.code" type="checkbox" class="sr-only" v-model="subscription.autoset" true-value="true" false-value="false" 
+                    <input disabled :id="'toogles'+subscription.code" type="checkbox" class="sr-only" v-model="subscription.autoset" true-value="true" false-value="false" 
                       @change="changeAutoSet(subscription)"/>
                     <div class="w-10 h-4 bg-gray-400 rounded-full shadow-inner"></div>
                     <div class="dot absolute w-6 h-6 bg-white rounded-full shadow -left-1 -top-1 transition"></div>
