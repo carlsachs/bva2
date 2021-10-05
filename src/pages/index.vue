3c0f26156f2a413923e3ca4dc02d0a8dfcd8672e
JSapp.me <herve76@gmail.com>
2021-10-05 17:11:10 -0600

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -58,7 +58,7 @@
                                 <button class="mx-1 my-2 text-sm items-center shadow-xl px-2 py-2 rounded-lg cursor-pointer">
                                     Tot. Trades <router-link :to="/strat/+row.id" class="font-bold text-green-500 font-semibold">{{ Number(row.countpnl).toFixed(0) }}</router-link>
                                 </button>
-                                <button class="mx-1 my-2 text-sm items-center shadow-xl px-2 py-2 rounded-lg cursor-pointer">
+                                <button v-if="row.max_concurrent" class="mx-1 my-2 text-sm items-center shadow-xl px-2 py-2 rounded-lg cursor-pointer">
                                     Max. Open Trades <router-link :to="/strat/+row.id" class="font-bold text-green-500 font-semibold">{{ row.max_concurrent }}</router-link>
                                 </button>
                                 <button class="mx-1 my-2 text-sm items-center shadow-xl px-2 py-2 rounded-lg cursor-pointer">
