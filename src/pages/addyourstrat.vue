2425647c50d28729783653623d2cd1d98527e65d
JSapp.me <herve76@gmail.com>
2021-11-25 09:47:21 -0600

    Update addyourstrat.vue

--- src/pages/addyourstrat.vue
+++ src/pages/addyourstrat.vue
@@ -15,7 +15,7 @@
         <div class="text-xl text-cyan-200 mt-10">
             <img class="mx-auto my-10 object-contain md:object-scale-down" alt="TradingView Alert Settings" src="/tv_alert_setting.png"/>
         </div>
-        <div class="text-xl text-cyan-200 mt-10">
+        <div class="text-xl font-bold text-cyan-200 mt-10">
             The format of the alert shall be:
         </div>
         <div class="text-green-400 mt-10">
@@ -29,16 +29,53 @@
                 <li><b class="text-green-400">your strategy name</b> is the name of your strategy, changing it will track a new strategy.</li>
                 <li><b class="text-green-400">action</b> could be <i>buy</i>, <i>sell</i>, <i>take</i> or <i>close</i>.</li>
                 <li><b class="text-green-400">ticker</b> should be a valid binance pair.</li>
-                <li><b class="text-green-400">position</b> is optional, it can be <i>flat</i>, <i>long</i> or <i>short</i>.</li>
+            </ul>
+        </div>
+        <div class="text-xl font-bold text-cyan-200 mt-10">
+            If you need more options:
+        </div>
+        <div class="text-green-400 mt-3">
+            <code>{{ format2 }}</code>
+        </div>
+        <div class="text-xl text-cyan-200 mt-5">
+            <ul class="list-disc list-inside">
+                <li><b class="text-green-400">market_position</b> can be <i>flat</i>, <i>long</i> or <i>short</i> (used for swing trade).</li>
+                <li><b class="text-green-400">extra_data</b> can be anything you want</li>
+                <li><b class="text-green-400">published</b> set it to true if you want your signal to be public.</li>
+                <li><b class="text-green-400">trade_size_pct</b> 0-100, the pourcentage of the trade size (used for DCA orders)</li>
+                <li><b class="text-green-400">trading_market</b> can be: spot, margin or futures</li>
             </ul>
         </div>
         <hr class="w-5 mx-auto border-blue-400 mt-6">
         <div class="text-xl text-cyan-200 mt-10">
-            <b>To add an auto trading subscription to your strat</b>, you decide its monthly price, <b>BVA takes 20%</b>.
+
+After you start sending us your signals, we track the performance of your strategy.  <br>
+As soon as your strategy enters the Top 10 Weekly, we can add an auto trading subscription to it.   <br>
+You decide its monthly price, <b>BVA takes 20%</b> of commission.  <br>
+We will need then the following information:  <br>
+<br>
+    <ul class="list-disc list-inside">
+        <li>a short description </li>
+        <li>a square logo (min. 200 x 200) </li>
+        <li>the max concurrent trades or the number of supported pairs </li>
+        <li>market type: SPOT, MARGIN or FUTURES </li>
+        <li>the monthly price for the sub (in USDT) </li>
+        <li>how many subs to sell at this initial price</li>
+    </ul>
+<br>
+<b>Once you strategy is listed</b>, here is how our user subscription and pay out process works:<br>
+<br>
+<ul class="list-disc list-inside" style="list-style-type:none;">
+        <li><b>1.</b> The user subscribes to your strategy.</li>
+        <li><b>2.</b> The user sends the USDT payment to the BVA wallet and the sale appears in your Sales dashboard.</li>
+        <li><b>3.</b> We send you the 80% as soon as we receive it and the new sale is marked as Paid Out in your Sales dashboard.</li>
+        <li><b>4.</b> If the user does not send the USDT within 24 hours, his subscription gets cancelled.</li>
+    </ul>
+<br>
         </div>
         <hr class="w-5 mx-auto border-blue-400 mt-6">
         <div class="text-xl text-cyan-200 mt-10">
-            <b>2.</b> For the <b>developers</b>, simply <b>POST</b> call our web hook, and use the same format for the <b>BODY</b>.
+            <b>2.</b> For <b>developers</b>, simply <b>POST</b> call our web hook, and use the same format for the <b>BODY</b>.
             <div class="w-200 mx-auto typing pl-2 mt-3">
                 <div class="text-left text-green-100 border-light-100 border-1 p-4">
                     <h2 class='font-bold text-cyan-200 mb-3'><u>Python Sample Code:</u></h2>
@@ -98,7 +135,8 @@ export default defineComponent({
 
     const state = reactive({
         auth0, 
-        format: "your_bva_key|your strategy name|{{strategy.order.action}}|{{ticker}}|{{strategy.market_position}}",
+        format: "your_bva_key|your strategy name|{{strategy.order.action}}|{{ticker}}",
+        format2: "your_bva_key|your strategy name|{{strategy.order.action}}|{{ticker}}|{{strategy.market_position}}|extra_data|published|trade_size_pct|trading_market",
     })
       
     return {
