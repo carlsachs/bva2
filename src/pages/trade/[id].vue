5e70f35b0fb288719f0abd6ba7cbd27b572d2f62
Herve Fulchiron <herve76@gmail.com>
2022-02-10 16:04:45 -0600

    rem logs

--- src/pages/trade/[id].vue
+++ src/pages/trade/[id].vue
@@ -367,15 +367,12 @@ export default defineComponent({
                 y: [ Number(price[1]), Number(price[2]), Number(price[3]), Number(price[4]) ]
               })
             
-              console.log(price[0], Number(signal.data[0].buy_time_2), Number(signal.data[0].buy_time_2), (price[0]+86400000))
-
               if ( price[0] < Number(signal.data[0].sell_time) && Number(signal.data[0].sell_time) < (price[0]+86400000) ) {
                 state.chartOptions.annotations.points[0].x = moment(price[0]).format('MMM DD HH:mm')
                 state.chartOptions.annotations.points[0].y = Number(signal.data[0].sell_price)
                 state.chartOptions.annotations.points[0].label.text = 'SELL'
               }
               if ( price[0] < Number(signal.data[0].buy_time) && Number(signal.data[0].buy_time) < (price[0]+86400000) ) {
-                //console.log("==========>BUY1", moment(price[0]).format('MMM DD HH:mm'), Number(signal.data[0].buy_price))
                 state.chartOptions.annotations.points[1].x = moment(price[0]).format('MMM DD HH:mm')
                 state.chartOptions.annotations.points[1].y = Number(signal.data[0].buy_price)
                 state.chartOptions.annotations.points[1].label.text = 'BUY'
@@ -386,7 +383,6 @@ export default defineComponent({
                 state.chartOptions.annotations.points[2].label.text = 'SELL2'
               }
               if ( price[0] < Number(signal.data[0].buy_time_2) && Number(signal.data[0].buy_time_2) < (price[0]+86400000) ) {
-                console.log("==========>BUY2", moment(price[0]).format('MMM DD HH:mm'), Number(signal.data[0].buy_price_2))
                 state.chartOptions.annotations.points[3].x = moment(price[0]).format('MMM DD HH:mm')
                 state.chartOptions.annotations.points[3].y = Number(signal.data[0].buy_price_2)
                 state.chartOptions.annotations.points[3].label.text = 'BUY2'
